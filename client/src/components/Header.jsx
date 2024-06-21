54cdde3c45c0e88647d937caa1662e83ee39038d
Mostafa Hassan <mo2stafa2002@gmail.com>
2024-06-21 22:31:21 +0300

    update the header and make the profile private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import logo from '../assets/Ticket-Rabbit-logo-wide.svg';
 import {Link} from 'react-router-dom'
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+  const {currentUser} = useSelector(state => state.user)
   return (
     <header className='bg-white shadow-sm'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-2'> {/*try different  p */}
@@ -23,10 +25,15 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to='/sign-in'>
-            <li className='text-slate-700 hover:text-green-500'>
-              Sign in
-            </li>
+          <Link to='/profile'>
+
+          {currentUser ? (
+            <img src={currentUser.avatar} alt="Profile" className="w-8 h-8 rounded-full cursor-pointer" />
+          ) : <li className='text-slate-700 hover:text-green-500'>
+                Sign in
+              </li>
+          }
+
           </Link>
         </ul>
       </div>
