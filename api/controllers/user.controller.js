cd5d17c630b7bdd691d9525689823dc44234f89e
Mostafa Hassan <mo2stafa2002@gmail.com>
2024-07-01 07:40:07 +0300

    create user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,35 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs"
+import { errorHandler } from "../utils/error.js";
+
 export const test = (req, res) => {
     res.send('hello world!');
+};
+
+export const updateUser = async (req, res, next) => {
+    if(req.user.id !== req.params.id) {
+        return next(errorHandler(403, 'You can update only your account!'));
+    }
+
+    try {
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10);
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: {
+                companyname: req.body.companyname,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar,
+            }
+        }, {new: true});
+
+        const {password, ...rest} = updatedUser._doc;
+
+        res.status(200).json(rest);
+
+    } catch (error) {
+        next(error);
+    }
 };
